a8b80f895dbaa38a8951c9656f646868adcb4aa6
jmhans <jmhans@hotmail.com>
2019-07-12 14:44:45 -0400

    upgrade complete

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -82,7 +82,7 @@
  */
 
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-
+import 'hammerjs/hammer';
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
