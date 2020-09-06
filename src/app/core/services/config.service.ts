ba9105ac1b0d1ca90284d27cd4ec0a56a1052a9e
jmhans <jmhans@hotmail.com>
2020-09-06 18:41:54 -0400

    Updated deadline

--- src/app/core/services/config.service.ts
+++ src/app/core/services/config.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ConfigService {
   nominate_end_time: object = new Date("2020-08-31T11:59:59")
-  bid_end_time: object = new Date("2020-09-06T23:59:59")
+  bid_end_time: object = new Date("2020-09-07T12:00:00Z")
     
   
   constructor() { }
